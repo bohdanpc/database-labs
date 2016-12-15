1d92c2fb287789049de2077957aa420aa43f24fc
Levunets Bohdan <bohdanpc@gmail.com>
2016-12-15 21:54:28 +0200

    sql script created

---
+++ lab2_bd_mine/DATABASES.sql
@@ -0,0 +1,23 @@
+use lab2db;
+
+drop table if exists client;
+drop table if exists time_tab;
+drop table if exists client_tab;
+drop table if exists bank_tab;
+
+create table client_tab (client_id int primary key auto_increment,
+					     client_name varchar(30), age int, is_working boolean);
+                     
+create table time_tab (time_id int primary key auto_increment,
+				       start_date time, end_time time);
+                       
+create table bank_tab (bank_id int primary key auto_increment,
+					   bank_name varchar(30), is_solvent boolean);
+          
+create table credit (credit_id int primary key auto_increment,
+					 client_id int, time_id int, bank_id int,
+					 summa decimal(6, 2), percentage decimal(3,1),
+					 currency varchar(3), 
+					 foreign key (client_id) references client_tab(client_id), 
+                     foreign key (time_id) references time_tab(time_id),
+                     foreign key (bank_id) references bank_tab(bank_id));
